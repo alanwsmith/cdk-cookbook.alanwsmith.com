933f68ec64c6b4b39b28442639432682d34223f9
Alan Smith <commit@alanwsmith.com>
2022-09-09 18:09:49 -0400

    showing names and correct connection ids

--- site/recipes/api_gateway_websocket_chat_app/assets/lambda-functions/web_page/index.js
+++ site/recipes/api_gateway_websocket_chat_app/assets/lambda-functions/web_page/index.js
@@ -1,62 +1,57 @@
 exports.handler = async (event) => {
-  console.log(`WEB PAGE`)
   const response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: `<!DOCTYPE html>
-  <html>
-    <head>
-      <style>
-        body {
-          background-color: #333;
-          color: #eee;
-        }
-      </style>
-      <script>
-        addEventListener('DOMContentLoaded', (event) => {
-          const socket = new WebSocket(
-            '${process.env.websocket_endpoint}'
-          )
-          socket.addEventListener('message', (event) => {
-            // console.log(event)
-            const message_text = JSON.parse(event.data).message
-            const connection_id = JSON.parse(event.data).connectionId
-            // console.log(message_text)
-            const newLi = document.createElement('li')
-            newLi.innerHTML = connection_id
-            newLi.innerHTML += '<br />'
-            newLi.innerHTML += message_text
-            const theUl = document.getElementById('the_messages')
-            theUl.appendChild(newLi)
-          })
-          document
-            .getElementById('submitter')
-            .addEventListener('click', (event) => {
-              const message_text = document.getElementById('message').value
-              const payload = JSON.stringify({
-                action: 'send_message',
-                message: JSON.stringify({ message: message_text }),
+    "isBase64Encoded": false,
+    "statusCode": 200,
+    "body": `<!DOCTYPE html>
+      <html>
+        <head>
+          <style>
+            body {
+              background-color: #333;
+              color: #eee;
+            }
+          </style>
+          <script>
+            addEventListener('DOMContentLoaded', (event) => {
+              const socket = new WebSocket(
+                '${process.env.websocket_endpoint}'
+              )
+              socket.addEventListener('message', (event) => {
+                const message_text = JSON.parse(event.data).message
+                const connection_id = JSON.parse(event.data).connectionId
+                const newLi = document.createElement('li')
+                newLi.innerHTML = `${connection_id}<br />${message_text}`
+                const theUl = document.getElementById('the_messages')
+                theUl.appendChild(newLi)
               })
-              socket.send(payload)
+              document
+                .getElementById('submitter')
+                .addEventListener('click', (event) => {
+                  const message_text = document.getElementById('message').value
+                  const payload = JSON.stringify({
+                    action: 'sendmessage',
+                    message: JSON.stringify({ message: message_text }),
+                  })
+                  socket.send(payload)
+                })
             })
-        })
-      </script>
-    </head>
-    <body>
-      <div>
-        <div>
-          <label for="message">Message: </label>
-          <input type="text" id="message" name="themessage" />
-          <button id="submitter">Send</button>
-        </div>
-        <ul id="the_messages"></ul>
-      </div>
-    </body>
-  </html>
-  `,
-    headers: {
-      'content-type': 'text/html',
-    },
-  }
-  return response
-}
+          </script>
+        </head>
+        <body>
+          <div>
+            <div>
+              <label for="message">Message: </label>
+              <input type="text" id="message" name="themessage" />
+              <button id="submitter">Send</button>
+            </div>
+            <ul id="the_messages"></ul>
+          </div>
+        </body>
+      </html>
+      `,
+    "headers": {
+      "content-type": "text/html"
+    }
+  };
+  return response;
+};
